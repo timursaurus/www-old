c7cf23cdf989db4720749dfe6a01a95e47d12d34
Timur Bolotov <bolotovtmr@gmail.com>
2022-06-23 16:31:34 +0600

    chore: nuxtrc4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   typescript: {
     strict: true,
-  }
-})
+  },
+  target: "static",
+  ssr: false,
+  modules: ["@nuxtjs/tailwindcss"],
+});
